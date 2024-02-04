6720bbbeeb0942253b94a7be3d7e32d21e34116f
Amendra <z1234146@wowcorp.com.au>
2024-02-04 18:10:56 +1100

    fix warnings

--- sequence-insights/test/SequenceInsights.Tests/Models/TestCase.cs
+++ sequence-insights/test/SequenceInsights.Tests/Models/TestCase.cs
@@ -3,7 +3,7 @@ namespace SequenceInsights.Tests.Models;
 // Define a class to represent a test case
 public class TestCase
 {
-    public string TestName { get; set; }
-    public string Input { get; set; }
-    public string Output { get; set; }
+    public string? TestName { get; set; }
+    public string? Input { get; set; }
+    public string? Output { get; set; }
 }
